fca0e978e4079d21af6cca51e322294908900d2b
unknown <alihuzaaifa8899@gmail.com>
2024-10-01 12:56:40 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// var hours = document.getElementById("hours");
+// var min = document.getElementById("min");
+// var sec = document.getElementById("sec");
+
+setInterval(() => {
+
+    var time = new Date(); 
+
+    var hours = document.getElementById("hours");
+    var min = document.getElementById("min");
+    var sec = document.getElementById("sec");
+
+if (time.getHours() < 12) {
+
+        hours.innerText = time.getHours();
+        min.innerText = time.getMinutes();
+        sec.innerText = time.getSeconds() +":" + "AM";
+        
+    }
+    else {
+        hours.innerText = time.getHours()-12;
+        min.innerText = time.getMinutes();
+        sec.innerText = time.getSeconds()+":" + "PM";
+
+    }
+
+}, 1000);
